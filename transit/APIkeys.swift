78cf0954edf11698bd2b8482d62c699647063277
Toby Applegate <up661724@myport.ac.uk>
2016-03-11 12:06:10 +0000

    added info.plist and taken out API key from APIKeys.swift

---
+++ transit/APIkeys.swift
@@ -0,0 +1,15 @@
+//
+//  APIkeys.swift
+//  transit
+//
+//  Created by Toby Applegate on 07/03/2016.
+//  Copyright © 2016 Toby Applegate. All rights reserved.
+//
+
+import Foundation
+
+class APIkeys {
+    
+    let googleMapsKey = "XXX"
+    
+}
